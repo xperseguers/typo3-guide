24a0959d55f6d444f30fc743b3bd375b64e6a9e5
boasbam <b.bamberger@aholics.de>
2014-09-24 16:37:18 +0200

    [FEATURE] Switched to LinkedIn Hopscotch
    Added some style improvements and demo content.

--- Resources/Public/Javascripts/guided-tour.js
+++ Resources/Public/Javascripts/guided-tour.js
@@ -1,101 +1,54 @@
-$(function () {
-    var $demo,
-    duration,
-    remaining,
-    tour;
-    $demo = $('#demo');
-    duration = 5000;
-    remaining = duration;
-    tour = new Tour({
-        onStart: function () {
-            return $demo.addClass('disabled', true);
-        },
-        onEnd: function () {
-            return $demo.removeClass('disabled', true);
-        },
-        debug: true,
+//function childLoaded() {var dom = childGetElementById('someid');}
+
+(function($) {
+
+    var tour = {
+        id: "testrun",
         steps: [
+            
             {
-                path: '/',
-                element: '#demo',
-                placement: 'bottom',
-                title: 'Welcome to Bootstrap Tour!',
-                content: 'Introduce new users to your product by walking them through it step by step.'
+                target: "typo3-topbar",
+                placement: "bottom",
+                title: "TYPO3 BODY",
+                content: "This is the BODY.",
+                highlight: false
             },
             {
-                path: '/',
-                element: '#usage',
-                placement: 'top',
-                title: 'A super simple setup',
-                content: 'Easy is better, right? The tour is up and running with just a few options and steps.'
+                target: "live-search-box",
+                placement: "left",
+                title: "TYPO3 Search",
+                content: "This is the pagetree of my page.",
+                highlight: true
             },
             {
-                path: '/',
-                element: '#license',
-                placement: 'top',
-                title: 'Best of all, it\'s free!',
-                content: 'Yeah! Free as in beer... or speech. Use and abuse, but don\'t forget to contribute!'
+                target: "typo3-logo",
+                placement: "bottom",
+                title: "TYPO3 Logo",
+                content: "This is the TYPO3 Logo.",
+                showPrevButton: true,
+                highlight: true
             },
             {
-                path: '/api',
-                element: '#options',
-                placement: 'top',
-                title: 'Flexibilty and expressiveness',
-                content: 'There are more options for those who want to get on the dark side.<br>\nPower to the people!',
-                reflex: true
+                target: "extdd-2",
+                placement: "right",
+                title: "TYPO3 <strong>Congratulations</strong>",
+                content: "This is the TYPO3 Pagetree, <br /> let's do <b>some</b> <i>fancy</i> HTML stuff here.",
+                highlight: true
             },
             {
-                path: '/api',
-                element: '#duration',
-                placement: 'top',
-                title: 'Automagically expiring step',
-                content: 'A new addition: make your tour (or step) completely automatic. You set the duration, Bootstrap\nTour does the rest. For instance, this step will disappear in <em>5</em> seconds.',
-                duration: 5000
-            },
-            {
-                path: '/api',
-                element: '#methods table',
-                placement: 'top',
-                title: 'A new shiny Backdrop option',
-                content: 'If you need to highlight the current step\'s element, activate the backdrop and you won\'t lose\nthe focus anymore!',
-                backdrop: true
-            },
-            {
-                path: '/api',
-                element: '#reflex',
-                placement: 'bottom',
-                title: 'Reflex mode',
-                content: 'Reflex mode is enabled, click on the text in the cell to continue!',
-                reflex: true
-            },
-            {
-                path: '/api',
-                title: 'And support for orphan steps',
-                content: 'If you activate the orphan property, the step(s) are shown centered in the page, and you can\nforget to specify element and placement!',
-                orphan: true,
-                onHidden: function () {
-                    return window.location.assign('/');
-                }
+                target: "ext-gen93",
+                placement: "bottom",
+                title: "Dings...",
+                content: "Bums...",
+                highlight: true
             }
         ]
-    }).init().start();
-    if (tour.ended()) {
-        $('<div class="alert alert-info alert-dismissable"><button class="close" data-dismiss="alert" aria-hidden="true">&times;</button>You ended the demo tour. <a href="#" data-demo>Restart the demo tour.</a></div>').prependTo('.content').alert();
-    }
-    $(document).on('click', '[data-demo]', function (e) {
-        e.preventDefault();
-        if ($(this).hasClass('disabled')) {
-            return;
-        }
-        tour.restart();
-        return $('.alert').alert('close');
+    };
+
+    // Start the tour! - once DOM is loaded completly
+    $(window).load(function(){
+        hopscotch.endTour(true); //clear session cookie
+        hopscotch.startTour(tour);
     });
-    $('html').smoothScroll();
-    return $('.gravatar').each(function () {
-        var $this,
-        email;
-        $this = $(this);
-        email = md5($this.data('email'));
-        return $(this).attr('src', 'http://www.gravatar.com/avatar/' + email + '?s=60');
-    });
-});
+
+})(TYPO3.jQuery);
--- Resources/Public/Javascripts/release-iframedom.js
+++ Resources/Public/Javascripts/release-iframedom.js
@@ -1,1 +1,8 @@
-release-iframedom.js
+// for each iframe the DOM needs to be passed to the parent
+// use name of the iframe to pass into var iframeID
+// insert JS to each BE frame
+
+parent.childGetElementById = function (iframeID){
+	return document.getElementById(iframeID);
+}
+parent.childLoaded();
